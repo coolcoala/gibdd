07ef3fffd1d8ba1c17794a123b926824104e6a9b
sova <pashokrulit@yandex.ru>
2024-07-04 05:36:31 +0300

    Initial Commit

---
+++ gibdd/gibddApp.swift
@@ -0,0 +1,17 @@
+//
+//  gibddApp.swift
+//  gibdd
+//
+//  Created by Sova on 04.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct gibddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
